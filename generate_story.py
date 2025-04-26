6616c97fe225b15745e0800e617484848082d366
Marques <marquesviniciusmelomartins@gmail.com>
2025-04-26 20:35:51 -0300

    Corrigir host e porta para producao no Render

--- generate_story.py
+++ generate_story.py
@@ -28,4 +28,6 @@ def generate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
